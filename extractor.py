8906aa1ac28dbd25aad228e02da20d06a6223e91
karismas <hubers001@gmail.com>
2024-02-23 21:15:55 -0500

    Switched to argparse, modified image displaying and pausing

--- extractor.py
+++ extractor.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import argparse
+
 import cv2
 import fitz
 from PIL import Image, ImageChops
@@ -10,10 +12,10 @@ from PyPDF2 import PdfReader
 # Add ability to only get diff and not extraction
 # Add ability to check between psm 1 and psm 6 and report difference as well as save diff for any problematic pages
 
-def getHighlightedText(original, highlighted, output, fromPage, toPage, scale, psm, saveDiffs):
+def getHighlightedText(original, highlighted, output, fromPage, toPage, scale, psm, saveDiffs, pauseDiffs):
 
-    path_to_tesseract = r"C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
-    pytesseract.tesseract_cmd = path_to_tesseract 
+    #path_to_tesseract = r"C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
+    #pytesseract.tesseract_cmd = path_to_tesseract 
 
     if fromPage != 0:
         fromPage -= 1
@@ -23,47 +25,45 @@ def getHighlightedText(original, highlighted, output, fromPage, toPage, scale, p
     originalPages = fitz.open(original)
     highlightedPages = fitz.open(highlighted)
 
-    with open(output, 'w') as file:
+    for i in range(fromPage, toPage):
 
-        file.write("")
+        originalPage = originalPages.load_page(i)
+        highlightedPage = highlightedPages.load_page(i)
+
+        originalPix = originalPage.get_pixmap(matrix=mat)
+        highlightedPix = highlightedPage.get_pixmap(matrix=mat)
+
+        originalPix.save('originalPage.jpg')
+        highlightedPix.save('highlightedPage.jpg')
+
+        originalImage = Image.open("originalPage.jpg")
+        highlightedImage = Image.open("highlightedPage.jpg")
+
+        diff = ImageChops.difference(originalImage, highlightedImage)
+
+        diffName = ''
+        if saveDiffs:
+            diffName = 'diff' + str(i + 1) + '.jpg'
+        else:
+            diffName = 'diff.jpg'
+
+        diff.save(diffName, 'JPEG')
+
+        diff = cv2.imread(diffName)
+        diff = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+
+        text = pytesseract.image_to_string(diff, config='--psm ' + str(psm))
+        text = text[:-1]
         
-        for i in range(fromPage, toPage):
-
-            file.writelines(["\n\n      PAGE " + str(i + 1) + "\n\n"])
-            
-            originalPage = originalPages.load_page(i)
-            highlightedPage = highlightedPages.load_page(i)
-
-            originalPix = originalPage.get_pixmap(matrix=mat)
-            highlightedPix = highlightedPage.get_pixmap(matrix=mat)
-
-            originalPix.save('originalPage.jpg')
-            highlightedPix.save('highlightedPage.jpg')
-
-            originalImage = Image.open("originalPage.jpg")
-            highlightedImage = Image.open("highlightedPage.jpg")
-
-            diff = ImageChops.difference(originalImage, highlightedImage)
-            print(diff.getbbox())
-
-            diffName = ''
-            if saveDiffs:
-                diffName = 'diff' + str(i + 1) + '.jpg'
-            else:
-                diffName = 'diff.jpg'
-
-            diff.save(diffName, 'JPEG')
-
-            diff = cv2.imread(diffName)
-            diff = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
-
-            text = pytesseract.image_to_string(diff, config='--psm ' + str(psm))
-            text = text[:-1]
-            
-            if text != "":
+        if text != "":
+            with open(output, 'a') as file:
+                file.writelines(["\n\n      PAGE " + str(i + 1) + "\n\n"])
                 file.writelines([text])
+                file.write("")
 
-            file.write("")
+        shownImage = cv2.imread('highlightedPage.jpg')
+        cv2.imshow('image', shownImage)
+        cv2.waitKey(0)
 
     originalPages.close()
     highlightedPages.close()
@@ -75,39 +75,33 @@ def getHighlightedText(original, highlighted, output, fromPage, toPage, scale, p
 
 def main(args):
 
-    original = "original.pdf"
-    highlighted = "highlighted.pdf"
-    output = "output.txt"
-    fromPage = 0
-    toPage = 0
-    scale = 5
-    psm = 1
-    saveDiffs = False
-    
-    for i, arg in enumerate(args):
-        if (arg == "--org"):
-            original = args[i + 1]
-        elif (arg == "--hl"):
-            highlighted = args[i + 1]
-        elif (arg == "--out"):
-            output = args[i + 1]
-        elif (arg == "--from"):
-            fromPage = int(args[i + 1])
-        elif (arg == "--to"):
-            toPage = int(args[i + 1])
-        elif (arg == "--scale"):
-            scale = int(args[i + 1])
-        elif (arg == "--psm"):
-            psm = int(args[i + 1])
-        elif (arg == "--diff"):
-            saveDiffs = True if args[i + 1] == "t" else False
+    toPage = args.toPage
 
     if toPage == 0:
         
-        reader = PdfReader(original)
+        reader = PdfReader(args.original)
         toPage = len(reader.pages)
 
-    getHighlightedText(original, highlighted, output, fromPage, toPage, scale, psm, saveDiffs)
+    getHighlightedText(args.original, args.highlighted, args.output, args.fromPage, toPage, args.scale, args.psm, False, args.diff)
+
+def parse_arguments():
+
+    parser = argparse.ArgumentParser(description="Extract highlights from a ReMarkable PDF.")
+    parser.add_argument('original', help="path of original PDF")
+    parser.add_argument('highlighted', help="path of highlighted PDF")
+    parser.add_argument('output', help="path of output file")
+    parser.add_argument('-s', '--scale', help="scale of the fitz matrix", default=5)
+    parser.add_argument('-p', '--psm', help="psm for pytesseract to use", default=1)
+    parser.add_argument('-f', '--fromPage', help="extraction starting page", default=0, type=int)
+    parser.add_argument('-t', '--toPage', help="extraction ending page", default=0, type=int)
+    parser.add_argument('-d', '--diff', help="display the image of the diff between the original and highlighted images", default=True)
+
+    args = parser.parse_args()
+    return args
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+
+    arguments = parse_arguments()
+    main(arguments) 
+
+#main(sys.argv[1:])
